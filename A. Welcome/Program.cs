9cd598c542b9c202e0fad8fa44a9a76436abfed0
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-15 14:32:51 +0300

    Add project files.

---
+++ A. Welcome/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name =Console.ReadLine();
+if (name == "Ready?")
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+Console.ReadLine();
